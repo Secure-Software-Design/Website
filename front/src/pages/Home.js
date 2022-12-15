b3509a835422d7f4666de104106aa6f6024c35b1
Thomas Lopez <thomas1.lopez@epitech.eu>
2022-12-15 20:43:04 +0100

    [ADD] matches

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -2,38 +2,155 @@ import Navbar from "../components/NavBar/Navbar";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import {getAuth, onAuthStateChanged} from "firebase/auth";
 
-const baseUrl = "http://127.0.0.1:8000/api/matchs"
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
 };
 
-
-
 export default function Home() {
-  let data;
+  const [matches, setMatches] = useState(null)
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    async function getMatches() {
+      let res = await fetch("http://127.0.0.1:8000/api/matchs", requestOptions)
+      console.log(res)
+      let data = await res.json();
+      setMatches(data)
+    }
 
+    getMatches()
 
-    async function getData() {
-    let res = await fetch("http://localhost:8000/api/matchs", requestOptions)
-    data = await res.json();
-    console.log(data)
+  }, [])
+  if (matches !== null) {
+    console.log(matches)
   }
 
-  getData();
-  
 
   return (
-    <div>
-      <Navbar/>
+    <div className="all">
+      <Navbar />
       <div className="final-container">
-        <div>
+        <div className="final-header">
+          FINAL
+        </div>
+        {matches !== null ? matches.final.map(match => (
+          <div className="match-card">
+            <div className="date">
+              {match.date_time}
+            </div>
+            <div className="match-info">
+              <img className="flag" src={match.team1_flag}/>
+              { match.team1 + " " + match.goal_team1 + " - " + match.goal_team2 + " " + match.team2}
+              <img className="flag" src={match.team2_flag}/>
+            </div>
+
+
+          </div>
+        )) : ""}
+      </div>
+      <hr className="line" />
+      <div className="final-container">
+        <div className="final-header">
+          SEMI-FINAL
+
+        </div>
+
+        <div className="semi-matches">
+        {matches !== null ? matches.semi_final.map(match => (
+          <div className="match-card">
+            <div className="date">
+              {match.date_time}
+            </div>
+            <div className="match-info">
+              <img className="flag" src={match.team1_flag}/>
+              { match.team1 + " " + match.goal_team1 + " - " + match.goal_team2 + " " + match.team2}
+              <img className="flag" src={match.team2_flag}/>
+            </div>
+
+
+          </div>
+        )) : ""}
+        </div>
+        <hr className="line" />
+
+
+
+      </div>
+      <div className="final-container">
+        <div className="final-header">
+          QUARTER-FINAL
+
+        </div>
+
+        <div className="quarter-matches">
+        {matches !== null ? matches.quarter_final.map(match => (
+          <div className="match-card">
+            <div className="date">
+              {match.date_time}
+            </div>
+            <div className="match-info">
+              <img className="flag" src={match.team1_flag}/>
+              { match.team1 + " " + match.goal_team1 + " - " + match.goal_team2 + " " + match.team2}
+              <img className="flag" src={match.team2_flag}/>
+            </div>
+
+
+          </div>
+        )) : ""}
+        </div>
+        
+      </div>
+       <hr className="line" />
+      <div className="final-container">
+        <div className="final-header">
+          ROUND OF 16
+
+        </div>
+
+        <div className="quarter-matches">
+        {matches !== null ? matches.round_of_16.map(match => (
+          <div className="match-card">
+            <div className="date">
+              {match.date_time}
+            </div>
+            <div className="match-info">
+              <img className="flag" src={match.team1_flag}/>
+              { match.team1 + " " + match.goal_team1 + " - " + match.goal_team2 + " " + match.team2}
+              <img className="flag" src={match.team2_flag}/>
+            </div>
+
+
+          </div>
+        )) : ""}
         </div>
       </div>
+      <hr className="line" />
+      <div className="final-container">
+        <div className="final-header">
+          FIRST STAGE
+
+        </div>
+
+        <div className="quarter-matches">
+        {matches !== null ? matches.first_stage.map(match => (
+          <div className="match-card">
+            <div className="date">
+              {match.date_time}
+            </div>
+            <div className="match-info">
+              <img className="flag" src={match.team1_flag}/>
+              { match.team1 + " " + match.goal_team1 + " - " + match.goal_team2 + " " + match.team2}
+              <img className="flag" src={match.team2_flag}/>
+            </div>
+
+
+          </div>
+        )) : ""}
+        </div>
+      </div>
+      <hr className="line" />
     </div>
   );
 }
